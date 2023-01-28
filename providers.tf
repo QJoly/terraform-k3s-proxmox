08ffd9f1649ade504d3b20ffdfd51a1458ee2976
QJoly <quentinjoly@thoughtless.eu>
2023-01-28 09:18:56 +0100

    Clear empty lines

--- providers.tf
+++ providers.tf
@@ -3,8 +3,6 @@ data "vault_generic_secret" "proxmox_host" {
   path = "kv/homelab"
 }
 
-
-
 provider "proxmox" {
   pm_api_url = data.vault_generic_secret.proxmox_host.data["proxmox_url"]
   pm_user    = data.vault_generic_secret.proxmox_host.data["proxmox_user"]
@@ -14,5 +12,3 @@ provider "proxmox" {
   pm_tls_insecure = "true"
   pm_parallel     = 5
 }
-
-
